e8b60343ce36bd4b271379fbc605d0e1a6ca9518
ahmetsuat67 <93923638+ahmetsuat67@users.noreply.github.com>
2022-02-20 22:12:02 +0300

    çalışma yayınlandı

---
+++ app.js
@@ -0,0 +1,69 @@
+let guessBox = document.getElementById("guessbox");
+let check = document.getElementById("check");
+let again = document.getElementById("again")
+let note = document.getElementById("note")
+let alarm = document.getElementById("alarm")
+let num = document.getElementById("guessnumber")
+let attempt = document.getElementById("attempt")
+let changeImg = document.getElementById("img")
+
+window.addEventListener("load", ()=> { 
+    let randomNumber = Math.round(Math.random() * 99 ); //Math.trunc yönteminde sayını küsuratını kesiyor
+    let counter = 0;
+    console.log(randomNumber);
+    again.style.display = "none"
+    again.addEventListener("click", ()=>{
+        location.reload()
+    })
+
+
+    check.addEventListener("click", ()=>{
+        if (guessBox.value >= 100){
+            alarm.innerText = "The number you entered must be between 1 and 100 please try again "
+            counter++;
+            num.value += guessBox.value + ",";
+            guessBox.value = "";
+            num.value += guessBox.value;
+            attempt.value = counter;
+        }else if (guessBox.value == randomNumber) { // sayı tahmin alanımıza girilen numara ile tahmin tuttuğunda 
+            counter++;
+            num.value += guessBox.value;
+            attempt.value = counter; //deneme sayımız
+            note.innerText = "CONGRATS"
+            check.style.display = "none"
+            again.style.display = "inline"
+            alarm.innerText =""
+            changeImg.src= "./assets/congratulations-celebrate.gif"
+        } else if (guessBox.value < randomNumber){ // sayı tahmin alanımıza girilen numara  tahmin numarasından küçük olduğunda 
+            counter++;
+            num.value += guessBox.value + ",";
+            guessBox.value = "";
+            attempt.value = counter;
+            note.innerText ="Enter a higher number" ;
+            alarm.innerText =""
+        } else if (guessBox.value > randomNumber){ // sayı tahmin alanımıza girilen numara  tahmin numarasından büyük olduğunda 
+            counter++;
+            num.value += guessBox.value + ",";
+            guessBox.value = "";
+            attempt.value = counter;
+            note.innerText ="Enter  a lower number" ;
+            alarm.innerText =""
+        }
+        if (counter == 10){
+            note.innerText= "Game OVER"
+            check.style.display = "none"
+            again.style.display = "inline"
+            changeImg.src= "./assets/robocop-says-game-over.gif" //yakaladığımız img tagını srcsini değiştiriyoruz
+        }
+    });
+
+    guessBox.addEventListener("keypress", (e) => { // enter bastığımızda çalışması için
+ 
+        if (e.key == "Enter") {
+            check.click();
+        }
+    });
+});
+
+//sonuca göre gif değiştir
+
