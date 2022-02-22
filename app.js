b531c2e155fd3f496dddf1cc89d59ed05d377cd6
ahmetsuat67 <93923638+ahmetsuat67@users.noreply.github.com>
2022-02-22 23:45:06 +0300

    değişiklikler

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ let alarm = document.getElementById("alarm")
 let num = document.getElementById("guessnumber")
 let attempt = document.getElementById("attempt")
 let changeImg = document.getElementById("img")
+let easy = document.getElementById("easy")
+let medium = document.getElementById("medium")
+let hard = document.getElementById("hard")
+let right = document.getElementById("right")
 
 window.addEventListener("load", ()=> { 
     let randomNumber = Math.round(Math.random() * 99 ); //Math.trunc yönteminde sayını küsuratını kesiyor
@@ -50,11 +54,28 @@ window.addEventListener("load", ()=> {
             alarm.innerText =""
         }
         if (counter == 10){
-            note.innerText= "Game OVER"
-            check.style.display = "none"
-            again.style.display = "inline"
-            changeImg.src= "./assets/robocop-says-game-over.gif" //yakaladığımız img tagını srcsini değiştiriyoruz
-        }
+                note.innerText= "Game OVER"
+                check.style.display = "none"
+                again.style.display = "inline"
+                alarm.innerText= `The Number is ${randomNumber}`
+                changeImg.src= "./assets/robocop-says-game-over.gif" //yakaladığımız img tagını srcsini değiştiriyoruz
+                alarm.style.color = "red"
+            }
+           
+      
+        // else if(hard.value == "hard"){
+        //     if (counter == 3){
+        //         note.innerText= "Game OVER"
+        //         check.style.display = "none"
+        //         again.style.display = "inline"
+        //         alarm.innerText= `The Number is ${randomNumber}`
+        //         changeImg.src= "./assets/robocop-says-game-over.gif" //yakaladığımız img tagını srcsini değiştiriyoruz
+        //         alarm.style.color = "red"
+        //         right.innerText = `Your Right to Try : 3`
+        //     }
+        // }
+      
+
     });
 
     guessBox.addEventListener("keypress", (e) => { // enter bastığımızda çalışması için
